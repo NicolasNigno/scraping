200877f414a1fbec4e0459ba95fc5b9948d57e42
NicolasNigno <nicolasnigno@gmail.com>
2020-05-20 16:17:17 -0500

    Update farmatodo.py

--- farmatodo.py
+++ farmatodo.py
@@ -10,3 +10,5 @@ import time
 import pandas as pd
 from price_parser import Price
 from bs4 import BeautifulSoup
+
+url = 'https://www.farmatodo.com.co/categorias/cuidado-personal/higiene-personal/proteccion-femenina'
