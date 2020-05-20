f4fe75bdb1ad2686129958fd5f4cd830b333fedb
NicolasNigno <nicolasnigno@gmail.com>
2020-05-20 18:17:49 -0500

    Update farmatodo.py

--- farmatodo.py
+++ farmatodo.py
@@ -10,8 +10,8 @@ Farmatodo scraper
 from selenium import webdriver
 import time
 import pandas as pd
+import numpy as np
 from price_parser import Price
-from bs4 import BeautifulSoup
 from selenium.webdriver.support.wait import WebDriverWait
 
 url = 'https://www.farmatodo.com.co/categorias/cuidado-personal/higiene-personal/proteccion-femenina'
@@ -32,5 +32,49 @@ while more:
     if browser.find_elements_by_xpath("//button[@class='cont-button-more my-5']")==[]: 
         more = False
 
-productos = browser.find_elements_by_xpath("//div[@class='card-ftd mb-3']")
-productos = [x.text for x in productos]
+    time.sleep(5)
+
+time.sleep(30)
+
+containers = browser.find_elements_by_xpath("//div[@class='card-ftd mb-3']")
+
+products = []
+descriptions = []
+prices = []
+units = []
+
+for items in containers:
+    try:
+        product = items.find_element_by_xpath('.//p[@class="text-title"]').text
+        products.append(product)
+    except:
+        products.append(np.nan)
+
+    try:
+        description = items.find_element_by_xpath('.//p[@class="text-description"]').text
+        descriptions.append(description)
+    except:
+        descriptions.append(np.nan)
+
+    try:
+        price = items.find_element_by_xpath('.//span[@class="text-price"]').text
+        prices.append(price)
+    except:
+        prices.append(np.nan)
+
+    try:
+        unit = items.find_element_by_xpath('.//p[@class="text-price-unit"]').text
+        units.append(unit)
+    except:
+        units.append(np.nan)
+
+dict_ = {'products': products, 'descriptions':descriptions, 'prices':prices, 'units':units}
+data_farmatodo = pd.DataFrame(dict_)
+
+def getvalue(x):
+    try:   
+        return Price.fromstring(x).amount_float
+    except: 
+        return None
+
+data_farmatodo['price_new'] = data_farmatodo['prices'].apply(lambda x: getvalue(x))
