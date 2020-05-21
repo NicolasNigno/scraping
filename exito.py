332e6b19caf66b2e67cd27c989420ec3aba4c433
Nicolás Niño <nicolasnino@habi.co>
2020-05-21 15:44:24 -0500

    update

--- exito.py
+++ exito.py
@@ -9,8 +9,47 @@ from selenium import webdriver
 import time
 import pandas as pd
 from price_parser import Price
+import numpy as np
+
+products = []
+prices_old = []
+prices_exito = []
+prices_new = []
+units = []
 
 ## Página 1 ##
+
+def get_items(containers):
+    for item in containers:
+        try:
+            product = item.find_element_by_xpath(".//span[@class='vtex-store-components-3-x-productBrand f6 fw5 shelfProductName search-result-exito-product-summary-name-product']").text
+            products.append(product)
+        except:
+            products.append(np.nan)
+
+        try:
+            price_new = item.find_element_by_xpath(".//div[@class='flex f5 fw5 pa0 flex items-end justify-start w-100 search-result-exito-vtex-components-other-selling-price exito-vtex-components-4-x-otherSellingPrice']/span[1]").text
+            prices_new.append(price_new)
+        except:
+            prices_new.append(np.nan)
+
+        try:
+            price_exito = item.find_element_by_xpath(".//div[@class='flex f5 fw5 pa0 flex items-center justify-start w-100 search-result-exito-vtex-components-allies-discount exito-vtex-components-4-x-alliedDiscountPrice']/span").text
+            prices_exito.append(price_exito)
+        except:
+            prices_exito.append(np.nan)
+
+        try:
+            price_old = item.find_element_by_xpath(".//span[@class='flex w-100 c-muted-2 mb0 fw5 t-mini ttn search-result-exito-vtex-components-list-price exito-vtex-components-4-x-priceTag exito-vtex-components-4-x-priceTagDel']/del/span").text
+            prices_old.append(price_old)
+        except:
+            prices_old.append(np.nan)
+
+        try:
+            unit = item.find_element_by_xpath(".//div[@class='exito-vtex-components-4-x-mainContainerProductPum']/div").text
+            units.append(unit)
+        except:
+            units.append(np.nan)
 
 url = 'https://www.exito.com/salud-y-belleza/higiene-intima'
 browser = webdriver.Chrome(executable_path=r'C:\Users\nicol\Downloads\chromedriver.exe')
@@ -18,10 +57,9 @@ browser.get(url)
 
 time.sleep(10)
 
-exito = browser.find_elements_by_xpath("//article[@class='vtex-product-summary-2-x-element pointer pt3 pb4 flex flex-column h-100']")
-productos = [x.text for x in exito]
+containers = browser.find_elements_by_xpath("//article[@class='vtex-product-summary-2-x-element pointer pt3 pb4 flex flex-column h-100']")
 
-time.sleep(2)
+get_items(containers)
 
 browser.quit()
 
@@ -39,11 +77,9 @@ while finished == False:
         time.sleep(20)
         if browser.find_elements_by_xpath("//article[@class='vtex-product-summary-2-x-element pointer pt3 pb4 flex flex-column h-100']") == []:
             finished = True
-            
-        exito = browser.find_elements_by_xpath("//article[@class='vtex-product-summary-2-x-element pointer pt3 pb4 flex flex-column h-100']")
-        productos_ = [x.text for x in exito]
         
-        productos = productos + productos_
+        containers = browser.find_elements_by_xpath("//article[@class='vtex-product-summary-2-x-element pointer pt3 pb4 flex flex-column h-100']")
+        get_items(containers)
         
         n+=1
         
@@ -52,34 +88,9 @@ while finished == False:
         browser.quit()
     except:
         pass
-      
-## Arreglar datos
 
-column_names = ['producto', 'precio 1', 'precio 2', 'unidades', 'info extra']
-
-datos_exito = pd.DataFrame(columns = column_names)
-
-productos_new = [x.split('\n') for x in productos]
-
-j=0
-for info in productos_new:
-   for i in info:
-       prices = [x for x in info if x.startswith("$")]
-       
-       try: datos_exito.loc[j, 'precio 1'] = prices[0]
-       except: pass
-       try: datos_exito.loc[j, 'precio 2'] = prices[1]
-       except: pass
-       
-       if ('$' not in i) and ('%' not in i) and ('Unidad' not in i) and ('¡Lo quiero!' not in i):
-           datos_exito.loc[j, 'producto'] = i
-       elif 'Unidad' in i:
-           datos_exito.loc[j, 'unidades'] = i
-       elif '%' in i:
-           datos_exito.loc[j, 'info extra'] = i
-           
-   j+=1
-    
+dict_ = {'products': products, 'prices_old':prices_old, 'prices_exito':prices_exito, 'prices_new':prices_new, 'units':units}
+data_exito = pd.DataFrame(dict_)
 
 def getvalue(x):
     try:   
@@ -87,25 +98,18 @@ def getvalue(x):
     except: 
         return None
 
-datos_exito['price 1'] = datos_exito['precio 1'].apply(lambda x: getvalue(x))
-datos_exito['price 2'] = datos_exito['precio 2'].apply(lambda x: getvalue(x))
+data_exito['prices_old_2'] = data_exito['prices_old'].apply(lambda x: getvalue(x))
+data_exito['prices_exito_2'] = data_exito['prices_exito'].apply(lambda x: getvalue(x))
+data_exito['prices_new_2'] = data_exito['prices_new'].apply(lambda x: getvalue(x))
 
 def getunidad(x):
     try:
-        x = x.replace('Unidad a $ ', '')
         x = x.replace(',', '.')
+        position = x.index('$')
+        x = x[position+1:].strip()
         return float(x)
     except:
         return None
 
-datos_exito['unidad'] = datos_exito['unidades'].apply(lambda x: getunidad(x))
+data_exito['units_2'] = data_exito['units'].apply(lambda x: getunidad(x))
 
-def getdescuento(x):
-    try:
-        x = x.replace('%', '')
-        x = float(x)
-        return x/100
-    except:
-        return None
-
-datos_exito['descuento'] = datos_exito['info extra'].apply(lambda x: getdescuento(x))
